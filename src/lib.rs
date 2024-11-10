62fb5afb108597f44a4cbe22da4881b7ba7adac6
zainkhatri <zainkhatri2560@gmail.com>
2024-11-09 21:07:40 -0800

    Spin the block twice like there ain't no where to park

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,39 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
+    entrypoint, entrypoint::ProgramResult,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
 
-// Define the instruction data structure
+// Define the data structure for a journal entry
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct JournalEntry {
     pub content_hash: String,
+    pub is_public: bool,
 }
 
-// Program entrypoint
+// Define the program's entrypoint
 entrypoint!(process_instruction);
 
-pub fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
+fn process_instruction(
+    program_id: &Pubkey,      // Public key of the program
+    accounts: &[AccountInfo], // Accounts involved in the instruction
+    instruction_data: &[u8],  // Serialized JournalEntry
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
-    let account = next_account_info(accounts_iter)?;
 
-    // Deserialize the instruction data
-    let journal_entry = JournalEntry::try_from_slice(instruction_data)?;
+    // Get the account of the person who initiated the transaction
+    let signer = next_account_info(accounts_iter)?;
 
-    // Log the content hash and author
-    msg!("Author: {}", account.key);
-    msg!("Content Hash: {}", journal_entry.content_hash);
+    // Deserialize the instruction data to get the JournalEntry
+    let journal_entry = JournalEntry::try_from_slice(instruction_data)
+        .map_err(|_| ProgramError::InvalidInstructionData)?;
 
-    // Business logic would go here (e.g., storing data on-chain)
+    // Here, you'd typically store the journal entry in an account or process it further
+    // Since this example focuses on transaction logging, we'll skip storage
+
+    // For demonstration, you might log the entry or perform other actions
 
     Ok(())
 }
